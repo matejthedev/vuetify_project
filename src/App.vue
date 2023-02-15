06b80f3edacddcc7891a5db3f4e3fac60fd4ae03
Matej Pavic <mpavic_ag@medable.com>
2023-02-15 11:19:08 +0100

    app layout components posititons fix

--- src/App.vue
+++ src/App.vue
@@ -1,51 +1,55 @@
 <template>
   
   <v-app id="inspire">
+    <v-navigation-drawer
+      v-model="drawer"
+      app
+    >
 
-    <v-app-bar>
-      <v-app-bar-nav-icon @click="drawer = !drawer">
-      </v-app-bar-nav-icon>
-      <v-toolbar-title>Application</v-toolbar-title>
-    </v-app-bar>
+    <v-list-item>
+      <v-list-item-content >
+        <v-list-item-title class="title">
+          Vuetify
+        </v-list-item-title>
+        <v-list-item-subtitle>
+          Todo app
+        </v-list-item-subtitle>
+      </v-list-item-content>
+    </v-list-item>
 
-      <v-navigation-drawer
-        v-model="drawer"
+    <v-divider></v-divider>
+
+    <v-list
+      nav
+      dense
+    >
+      <v-list-item
+        v-for="item in items"
+        :key="item.title"
+        :to="item.to"
+        link
       >
-        <v-list-item>
-          <v-list-item-content >
-            <v-list-item-title class="title">
-              Vuetify
-            </v-list-item-title>
-            <v-list-item-subtitle>
-              Todo app
-            </v-list-item-subtitle>
-          </v-list-item-content>
-        </v-list-item>
+        <v-list-item-icon>
+          <v-icon>{{ item.icon }}</v-icon>
+        </v-list-item-icon>
+        <v-list-item-content>
+          <v-list-item-title>
+            {{ item.title }}
+          </v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+    </v-list>
 
-        <v-divider></v-divider>
+  </v-navigation-drawer> 
 
-        <v-list
-          dense
-          nav
-        >
-          <v-list-item
-            v-for="item in items"
-            :key="item.title"
-            :to="item.to"
-            link
-          >
-            <v-list-item-icon>
-              <v-icon>{{ item.icon }}</v-icon>
-            </v-list-item-icon>
-            <v-list-item-content>
-              <v-list-item-title>
-                {{ item.title }}
-              </v-list-item-title>
-            </v-list-item-content>
-          </v-list-item>
+  <v-app-bar
+  app
+  >
+    <v-app-bar-nav-icon @click="drawer = !drawer">
+    </v-app-bar-nav-icon>
+    <v-toolbar-title>Application</v-toolbar-title>
+  </v-app-bar>
 
-        </v-list>
-      </v-navigation-drawer> 
 
     <v-main>
       <router-view></router-view>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,33 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+      Excepturi et est facere commodi perferendis ipsa error voluptatibus soluta alias adipisci, 
+      inventore ducimus sed accusantium maxime ipsam suscipit illum quidem vero reiciendis assumenda 
+      ipsum! Maxime veritatis magnam, aliquam recusandae quo asperiores magni minima? 
+      Minus velit veritatis, neque laudantium at illum quasi?
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+      Excepturi et est facere commodi perferendis ipsa error voluptatibus soluta alias adipisci, 
+      inventore ducimus sed accusantium maxime ipsam suscipit illum quidem vero reiciendis assumenda 
+      ipsum! Maxime veritatis magnam, aliquam recusandae quo asperiores magni minima? 
+      Minus velit veritatis, neque laudantium at illum quasi?
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+      Excepturi et est facere commodi perferendis ipsa error voluptatibus soluta alias adipisci, 
+      inventore ducimus sed accusantium maxime ipsam suscipit illum quidem vero reiciendis assumenda 
+      ipsum! Maxime veritatis magnam, aliquam recusandae quo asperiores magni minima? 
+      Minus velit veritatis, neque laudantium at illum quasi?
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+      Excepturi et est facere commodi perferendis ipsa error voluptatibus soluta alias adipisci, 
+      inventore ducimus sed accusantium maxime ipsam suscipit illum quidem vero reiciendis assumenda 
+      ipsum! Maxime veritatis magnam, aliquam recusandae quo asperiores magni minima? 
+      Minus velit veritatis, neque laudantium at illum quasi?
+    </p>
   </div>
 </template>
