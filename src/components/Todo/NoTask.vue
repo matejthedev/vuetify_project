1a9ee79a7da40be49f8d5e5128ff679f29b75045
Matej Pavic <mpavic_ag@medable.com>
2023-02-16 15:35:12 +0100

    refactoring todo view into components

---
+++ src/components/Todo/NoTask.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="primary--text no-task">
+      <v-icon
+        size=100
+        color="primary"
+      >
+        mdi-check
+      </v-icon>
+      No tasks
+    </div>
+</template>
+
+<style lang="scss">
+  .no-task {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    opacity: 0.75;
+  }
+</style>
---
+++ src/components/Todo/TaskList.vue
@@ -0,0 +1,36 @@
+<template>
+      <v-list
+      class="pa-0"
+      flat
+    >
+      <div v-for="task in $store.state.tasks">
+        <v-list-item
+          @click="$store.commit('taskDone', task.id)"
+          :class="{ 'blue lighten-5' : task.done }"
+        >
+          <template v-slot:default>
+            <v-list-item-action>
+              <v-checkbox
+                :input-value="task.done"
+                color="primary"
+              ></v-checkbox>
+            </v-list-item-action>
+
+            <v-list-item-content>
+              <v-list-item-title 
+                :class="{ 'text-decoration-line-through' : task.done }"
+              >{{ task.name }}</v-list-item-title>
+            </v-list-item-content>
+            <v-list-item-action>
+              <v-btn icon
+              @click.stop = "$store.commit('deleteTask', task.id)"
+              >
+                <v-icon color="primary lighten-1">mdi-delete</v-icon>
+              </v-btn>
+            </v-list-item-action>
+          </template>
+        </v-list-item>
+        <v-divider></v-divider>
+      </div>
+    </v-list>
+</template>
